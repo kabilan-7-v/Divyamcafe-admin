023db45590bb53342cc267b3f1e1ed2f010d1932
kabilan-7-v <kabilanvelmani@gmail.com>
2025-04-01 22:17:59 +0530

    changes

--- src/Feedbackmanagement.jsx
+++ src/Feedbackmanagement.jsx
@@ -59,14 +59,14 @@ function FeedbackManagement() {
         {loading ? (
                <div className='h-100'> <p className="text-center text-black text-4xl mt-25">Loading...</p></div>
 
-      ) : error ? (
+      ) :testimonials.length === 0 ? (
+        <div className="h-100 flex flex-col justify-center items-center"> <h1 className="text-black text-2xl text-center mt-5">
+           NO FEEDBACK FOUND
+         </h1>
+         </div>
+       ) : error ? (
         <p className="text-center text-red-500 mt-5">{error}</p>
-      ) : testimonials.length === 0 ? (
-       <div className="h-100 flex flex-col justify-center items-center"> <h1 className="text-black text-2xl text-center mt-5">
-          NO FEEDBACK FOUND
-        </h1>
-        </div>
-      ) : (
+      )  : (
         <div className="grid grid-cols-4 gap-4">
           {testimonials.map((item, index) => (
             <div key={item._id || index} className="mt-5 flex-basis-[15%]">
